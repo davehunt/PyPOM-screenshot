e06320dfae04f222ca15bd800eea67a2ac9d48d3
Dave Hunt <dave.hunt@gmail.com>
2018-08-13 19:16:06 +0100

    Update for PyPOM 2.1.0

--- pypom_screenshot/plugin.py
+++ pypom_screenshot/plugin.py
@@ -2,10 +2,14 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from pypom import hookimpl
 
+
+@hookimpl
 def pypom_after_wait_for_page_to_load(page):
     page.selenium.get_screenshot_as_file(page.__class__.__name__ + '.png')
 
 
+@hookimpl
 def pypom_after_wait_for_region_to_load(region):
     region.root.screenshot(region.__class__.__name__ + '.png')
